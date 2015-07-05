d765197bdc6683d01c2e849111f6ee93d47a43d9
Heikki Hokkanen <hoxu@users.sf.net>
2015-07-05 17:15:29 +0300

    Add tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 Heikki Hokkanen <hoxu at users.sf.net>
+# License: GPLv3
+import unittest
+
+class TestGig(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
